f61b14801bf962ff4aa774d86194acdb72654a96
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2023-02-02 00:33:31 -0500

    Use environment variable in Config class

--- config.py
+++ config.py
@@ -2,5 +2,4 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    UPLOAD_FOLDER = 'uploads/'
-    
+    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', './uploads')
